41b7caed0a078470e0452af2dd954fc9e870561b
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-01 15:26:49 +0200

    solution of lab #1

---
+++ Lab01/lab01.py
@@ -0,0 +1,68 @@
+
+'Question 2 count vowel characters'
+
+# sentence = input("Enter your sentence: ")
+
+# count = 0
+
+# for char in sentence:
+#     if ['a', 'e', 'i', 'o', 'u'].count(char):
+#         count = count+1
+# print(count)
+
+
+'Question 3 sort user input list'
+
+# numbers = list()
+# for i in range(5):
+#     numbers.append(int(input()))
+# numbers.sort()
+# print ("Ascending: ",numbers)
+# numbers.reverse()
+# print("Descending: ",numbers)
+
+'Question 4'
+
+# str = input("Enter your string: ")
+
+# print(str.count('iti'))
+
+
+'Question 5'
+
+# str = input("Enter your string: ")
+
+# for v in ['a','e','i','o','u']:
+#     str = str.replace(v,'')
+# print(str)
+
+'Question 6'
+
+# str = input("Enter your string: ")
+
+# print("(i) is located at index: ",str.index('i'))
+
+
+'Questin 7'
+
+# number = int(input("Enter your limit: "))
+
+# for x in range(1, number+1):
+#     print(f'table {x}')
+#     print('------------------------')
+#     for y in range(1, x+1):
+#         print(f"{x} x {y} = {x*y}")
+#     print('------------------------');
+
+
+'Question 8'
+
+
+# for x in range(5):
+#     spaces = ''
+#     stars = ''
+#     for y in range(5-x+1):
+#         spaces+=' '
+#     for y in range(x+1):
+#         stars+='*'
+#     print(spaces,stars)
