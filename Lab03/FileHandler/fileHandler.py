188bd2f1482bfe2720c5bf99c78077d02a25860b
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-04 15:41:07 +0200

    create file handling methods

--- Lab03/FileHandler/fileHandler.py
+++ Lab03/FileHandler/fileHandler.py
@@ -1,2 +1,40 @@
 
 
+
+from time import time
+
+
+def write(fileName, line):
+    try:
+        file = open(fileName,"a")
+    except Exception as e:
+        print(e)
+    else:
+        file.write(line)
+        file.close()
+        return True
+    
+
+def writeAll(fileName, lines):
+    try:
+        file = open(fileName,"a")
+    except Exception as e:
+        print(e)
+    else:
+        file.writelines(lines)
+        file.close()
+        return True
+
+
+def toRow(data: dict):
+    row = ''
+    for value in data.values():
+        row+=f'{value}:'
+    row = row[:-1] + '\n'
+    return row
+
+
+def insertUser(user):
+    user['id'] = int(time())
+    row = toRow(user)
+    return write('./Lab03/DB/user.txt',row), user['id']
