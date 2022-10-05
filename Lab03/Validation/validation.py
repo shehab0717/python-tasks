1ed757ab9c3cd06c5ee506dce7228c2fbe2f9648
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-05 16:56:12 +0200

    add validation functions (number, date)

--- Lab03/Validation/validation.py
+++ Lab03/Validation/validation.py
@@ -32,3 +32,18 @@ def validateEmail(message = "Enter your email > "):
     if re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$',email):
         return email
     return validateEmail("Incorrect email!! > ")
+
+def validateNumber(message = " > "):
+    number = input(message)
+    if number.isdigit():
+        return number
+    print("Invalid number!")
+    return validateNumber("Invalid number > ")
+
+
+dateRegex = '^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$'
+def validateDate(message = "date > "):
+    date = input(message)
+    if True:
+        return date
+    return validateDate("Date isn't valid > ")
