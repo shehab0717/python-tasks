c05b52a2cb8541b22ebd3f4a8df4ba6e1cfb3c6f
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-05 16:07:41 +0200

    enhance filehandler service

--- Lab03/FileHandler/fileHandler.py
+++ Lab03/FileHandler/fileHandler.py
@@ -1,37 +1,95 @@
 
+import os.path
 
-
-from time import time
 
 _DB = './Lab03/DB'
 
-def write(fileName, line):
+
+def filePath(fileName):
+    return f'{_DB}/{fileName}'
+
+
+def checkFile(fileName, fileHeader):
+    if not os.path.exists(filePath(fileName)):
+        try:
+            file = open(filePath(fileName), 'w')
+        except Exception as e:
+            print(e)
+            return False
+        else:
+            file.write(toRow(fileHeader))
+            file.close()
+            return True
+    return True
+
+
+def write(fileName, fileHeader, line):
+    if checkFile(fileName, fileHeader):
+        try:
+            file = open(filePath(fileName), "a")
+        except Exception as e:
+            print(e)
+            return False
+        else:
+            file.write(line)
+            file.close()
+            return True
+    return False
+
+
+def writeAll(fileName, fileHeader, lines):
+    if checkFile(fileName, fileHeader):
+        try:
+            file = open(filePath(fileName), "a")
+        except Exception as e:
+            print(e)
+        else:
+            file.writelines(lines)
+            file.close()
+            return True
+
+
+def getFileHeader(fileName):
     try:
-        file = open(f'{_DB}/{fileName}',"a")
+        file = open(filePath(fileName), 'r')
     except Exception as e:
         print(e)
+        return None
     else:
-        file.write(line)
+        header = file.readline()
         file.close()
-        return True
-    
+        return header
 
-def writeAll(fileName, lines):
+def assignWithHeader(row, fileHeader):
+    fields = row.split(':')
+    cols = fileHeader.split(':')
+    data = dict()
+    for i in range(len(fields)):
+        data[cols[i]] = fields[i]
+    return data
+        
+
+
+def find(fileName, rule):
     try:
-        file = open(f'{_DB}/{fileName}',"a")
+        file = open(filePath(fileName), 'r')
     except Exception as e:
         print(e)
+        return None
     else:
-        file.writelines(lines)
+        lines = file.readlines()
+        fileHeader = getFileHeader(fileName)
+        for line in lines:
+            data = assignWithHeader(line, fileHeader)
+            if rule(data):
+                file.close()
+                return data
         file.close()
-        return True
+        return None
 
-
-def toRow(data: dict):
+def toRow(data):
     row = ''
-    for value in data.values():
-        row+=f'{value}:'
+    for value in data:
+        row += f'{value}:'
     row = row[:-1] + '\n'
     return row
-
-
