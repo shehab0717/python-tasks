c82279f0c3286b74e3a43557b68527ba4028307d
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-05 16:08:31 +0200

    add search user function to the user services

--- Lab03/Services/user_services.py
+++ Lab03/Services/user_services.py
@@ -1,11 +1,20 @@
 
 
 
+from pyclbr import Function
 import FileHandler.fileHandler as fileHandler
 from time import time
 
+fileHeader = ('id','first name', 'last name', 'email', 'password', 'phone number')
 
-def insertUser(user):
+
+def insertUser(user: dict):
     user['id'] = int(time())
-    row = fileHandler.toRow(user)
-    return fileHandler.write('user.txt',row), user['id']
+    row = fileHandler.toRow(user.values())
+    success = fileHandler.write('user.txt', fileHeader, row)
+    if success:
+        return success, user
+    return False, -1
+
+def search(rule: Function):
+    return fileHandler.find('user.txt',rule)
