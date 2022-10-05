5f5ce406398fc2f05cac82ba6a0da0e19a8866c2
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-05 16:57:04 +0200

    implement app interface, new project function

--- Lab03/app.py
+++ Lab03/app.py
@@ -1,11 +1,45 @@
 
 
+from time import time
 import auth
+import Validation.validation as vf
+import Services.project_services as projectServices
 
+def displayAllProjects(userId):
+    pass
+
+fields = (["title", vf.validateString],
+          ["details", vf.validateString],
+          ["target", vf.validateNumber], 
+          ["start date", vf.validateDate], 
+          ["end date", vf.validateDate])
+
+def newProject(userId):
+    project = dict()
+    project['id'] = int(time())
+    project['userId'] = userId
+    for field, validator in fields:
+        data = validator(message=f"{field} > ")
+        project[field] = data
+    success = projectServices.insert(project)
+    if success:
+        print(project)
+    else: 
+        print('Error happened!!')
+    return project
 
 def app():
     authinticated, user = auth.start()
-    print(f"{user}")
+    if authinticated:
+        print(f"----------- welcome {user['first name']} ------------")
+        print('1- New project \n2- list all projects \n3- exit')
+        choice = input("> ")
 
-
+        match choice:
+            case "1":
+                newProject(user['id'])
+            case _:
+                app()
+    else:
+        exit()
 app()
