8e4ad9e6e18ddef8bf2aa67343a2c7b900de6bc8
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-10-06 13:39:50 +0200

    add (update and deleted) options in the projects menue

--- Lab03/app.py
+++ Lab03/app.py
@@ -1,17 +1,59 @@
 
 
+from pyclbr import Function
 from time import time
 import auth
 import Validation.validation as vf
 import Services.project_services as projectServices
 
+updatableFields = (["title", vf.validateString], ["details", vf.validateString], [
+                   "target", vf.validateNumber], ["start date", vf.validateDate], ["end date", vf.validateDate])
+
+
+def updateField(filedName, validator: Function):
+    response = input(f"Update {filedName}? (y/n) > ")
+    if response == 'y' or response == 'Y':
+        return validator(message=f"{filedName} > ")
+    return None
+
+
 def updateProject():
     projectId = input("Enter the project id: ")
+
+    def rule(data):
+        return data['id'] == projectId
+    project = projectServices.find(rule)
+    if not project:
+        print("Project not found")
+        return False
+    for field in updatableFields:
+        fieldName = field[0]
+        validator = field[1]
+        updated = updateField(fieldName, validator)
+        if updated:
+            project[fieldName] = updated
+    updated = projectServices.update(project)
+    if updated:
+        print("Updated successfully")
+        return True
+    else:
+        print("Couldn't update project!!")
     return False
+
 
 def deleteProject():
     projectId = input("Enter the project id: ")
+    response = input("Are you sure? (y/n) > ")
+    if response == 'y' or response == 'Y':
+        deleted = projectServices.delete(projectId)
+        if deleted:
+            print("project deleted successfully")
+            return True
+        else:
+            print("Couldn't delete the project, try again later!")
+            return False
     return False
+
 
 def projectsMenue():
     print("1- Update\n2- Delete\n3- back")
@@ -40,7 +82,7 @@ def displayAllProjects(userId):
               f"Start date: {project['start date']}\nEnd date: {project['end date']}\n"
               "----------------------------------------------------------------------")
     projectsMenue()
-    
+
 
 fields = (["title", vf.validateString],
           ["details", vf.validateString],
@@ -63,11 +105,14 @@ def newProject(userId):
         print('Error happened!!')
     return project
 
+
 authinticated = False
 user = None
+
+
 def app():
     global authinticated, user
-    if not authinticated: 
+    if not authinticated:
         authinticated, user = auth.start()
     if authinticated:
         print(f"----------- welcome {user['first name']} ------------")
